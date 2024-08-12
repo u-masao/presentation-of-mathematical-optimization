edc0552f0e515bc0cd4f80848235e490561b7cb7
u-masao <4973920+u-masao@users.noreply.github.com>
2024-08-12 09:21:09 +0900

    debug mode

--- src/generate_scenario.py
+++ src/generate_scenario.py
@@ -53,6 +53,6 @@ def main(**kwargs):
 
 if __name__ == "__main__":
     log_fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
+    logging.basicConfig(level=logging.DEBUG, format=log_fmt)
     load_dotenv()
     main()
